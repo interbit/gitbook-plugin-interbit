1b5b5d65966fea4aaf4f6ff4ac78992383995bb6
Ewan Edwards <ewan@blockchaintechltd.com>
2018-08-27 13:34:40 -0700

    Improve debug output to accept multiple args, and use util.inspect to unroll objects.

--- scripts/_debug.js
+++ scripts/_debug.js
@@ -1,13 +1,21 @@
 const ansi  = require('ansi-escape-sequences')
+const util  = require('util')
 const color = ansi.style
 
 var DEBUG  = false
 var PREFIX = 'DEBUG'
 
-const out = (msg) => {
+const out = (...msgs) => {
   const d = module.exports.DEBUG
   const p = module.exports.PREFIX
-  if (d) console.log(`${color.blue}${p}: ${msg}${color.reset}`)
+  if (d) {
+    for (let m of msgs) {
+      if (typeof m !== "string") {
+        m = util.inspect(m, { showHidden: false, depth: null })
+      }
+      console.log(`${color.blue}${p}: ${m}${color.reset}`)
+    }
+  }
 }
 
 module.exports = { out, DEBUG, PREFIX }
